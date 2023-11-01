7f55e44da75bea173710075e72105a496fa74c01
jose_d <jose-d@users.noreply.github.com>
2023-11-01 18:56:08 +0100

    Update build_slurm.sh

--- .github/scripts/build_slurm.sh
+++ .github/scripts/build_slurm.sh
@@ -2,7 +2,11 @@
 
 # some tests
 set -x
+
+dnf -y install ${GITHUB_WORKSPACE}/pmix_rpms/*.rpm
           
 echo "PMIX_RELTAG: ${SLURM_RELTAG}, PMIX_VERSION: ${SLURM_VERSION}"
 mkdir -p ${HOME}/rpmbuild/SOURCES/
-dnf -y install ${GITHUB_WORKSPACE}/pmix_rpms/*.rpm
+cp ${GITHUB_WORKSPACE}/slurm-*.tar.bz2 $HOME/rpmbuild/SOURCES/
+
+set +x
