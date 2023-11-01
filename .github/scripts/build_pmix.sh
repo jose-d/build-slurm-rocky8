81db942098ebae690605b8e3c7e9835e578d219f
jose_d <jose-d@users.noreply.github.com>
2023-11-01 18:21:39 +0100

    Update build_pmix.sh

--- .github/scripts/build_pmix.sh
+++ .github/scripts/build_pmix.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# some tests
-set -x
-echo "Identity"
-cat /identity
-          
+        
 echo "PMIX_RELTAG: ${PMIX_RELTAG}, PMIX_VERSION: ${PMIX_VERSION}"
 
 mkdir -p ${HOME}/rpmbuild/SOURCES/
@@ -15,7 +10,9 @@ sed -i "s/^Release.*$/Release: ${PMIX_RELTAG}%{?dist}/g" pmix-*/contrib/pmix.spe
 
 cat pmix-*/contrib/pmix.spec
 
-set +x 
+set -x
 rpmbuild --define 'build_all_in_one_rpm 0' --define 'configure_options --disable-per-user-config-files' -ba ./pmix-*/contrib/pmix.spec
 mkdir ${GITHUB_WORKSPACE}/rpms
-cp ${HOME}/rpmbuild/RPMS/x86_64/pmix-*.rpm ${GITHUB_WORKSPACE}/rpms/
+cp ${HOME}/rpmbuild/RPMS/x86_64/*.rpm ${GITHUB_WORKSPACE}/rpms/
+
+set -x
