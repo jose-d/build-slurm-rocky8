c293db555ef56a55d4409b3378d4cf06333f2eb4
jose_d <jose-d@users.noreply.github.com>
2023-11-01 19:20:56 +0100

    Update build_slurm.sh

--- .github/scripts/build_slurm.sh
+++ .github/scripts/build_slurm.sh
@@ -9,4 +9,18 @@ echo "PMIX_RELTAG: ${SLURM_RELTAG}, PMIX_VERSION: ${SLURM_VERSION}"
 mkdir -p ${HOME}/rpmbuild/SOURCES/
 cp ${GITHUB_WORKSPACE}/slurm-*.tar.bz2 $HOME/rpmbuild/SOURCES/
 
+
+rpmbuild --define '_with_nvml --with-nvml=/usr/local/cuda/targets/x86_64-linux/' \
+          --with pam \
+          --with slurmrestd \
+          --with hwloc \
+          --with lua \
+          --with mysql \
+          --with numa \
+          --with pmix \
+          -ba ./slurm-*/slurm.spec
+
+mkdir ${GITHUB_WORKSPACE}/rpms
+cp ${HOME}/rpmbuild/RPMS/x86_64/slurm-*.rpm ${GITHUB_WORKSPACE}/rpms/
+
 set +x
