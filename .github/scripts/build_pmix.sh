5f1962392080d7c4c4af577a14a721701b2e6ae7
Dvořáček Josef <jose@fzu.cz>
2023-11-02 16:57:29 +0100

    use global vars and releasetag instead of per-job

--- .github/scripts/build_pmix.sh
+++ .github/scripts/build_pmix.sh
@@ -2,8 +2,8 @@
         
 echo "PMIX_RELTAG: ${PMIX_RELTAG}, PMIX_VERSION: ${PMIX_VERSION}"
 
-mkdir -p ${HOME}/rpmbuild/SOURCES/
-wget https://github.com/openpmix/openpmix/releases/download/v${PMIX_VERSION}/pmix-${PMIX_VERSION}.tar.bz2
+mkdir -p "${HOME}/rpmbuild/SOURCES/"
+wget "https://github.com/openpmix/openpmix/releases/download/v${PMIX_VERSION}/pmix-${PMIX_VERSION}.tar.bz2"
 tar -xf ./pmix*
 cp pmix-*.tar.bz2 ${HOME}/rpmbuild/SOURCES/
 sed -i "s/^Release.*$/Release: ${PMIX_RELTAG}%{?dist}/g" pmix-*/contrib/pmix.spec
@@ -12,7 +12,7 @@ cat pmix-*/contrib/pmix.spec
 
 set -x
 rpmbuild --define 'build_all_in_one_rpm 0' --define 'configure_options --disable-per-user-config-files' -ba ./pmix-*/contrib/pmix.spec
-mkdir ${GITHUB_WORKSPACE}/rpms
+mkdir "${GITHUB_WORKSPACE}/rpms"
 cp ${HOME}/rpmbuild/RPMS/x86_64/*.rpm ${GITHUB_WORKSPACE}/rpms/
 
 set -x
--- .github/scripts/build_slurm.sh
+++ .github/scripts/build_slurm.sh
@@ -6,7 +6,7 @@ set -x
 dnf -y install ${GITHUB_WORKSPACE}/pmix_rpms/*.rpm
           
 echo "PMIX_RELTAG: ${SLURM_RELTAG}, PMIX_VERSION: ${SLURM_VERSION}"
-mkdir -p ${HOME}/rpmbuild/SOURCES/
+mkdir -p "${HOME}/rpmbuild/SOURCES/"
 cp ${GITHUB_WORKSPACE}/slurm-*.tar.bz2 $HOME/rpmbuild/SOURCES/
 
 
